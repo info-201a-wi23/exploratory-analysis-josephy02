0a042ef979dec504885cdfca8f72562ba5343022
kylegrubb <kgrubb3@uw.edu>
2023-02-21 02:09:35 -0800

    edit table

--- table.R
+++ table.R
@@ -1,4 +1,4 @@
-master_table <- merge(maindf, my_df, Cities)
+master_table <- merge(maindf, my_df, cities)
 colnames(master_table)[5] = "Vacancy Rate"
 colnames(master_table)[3] = "Days on market period"
 colnames(master_table)[4] = "Vacancy Rate 6 month period start"
