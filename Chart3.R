ab0095ddc0953d30f6ca8b61f606262042b81d54
kylegrubb <kgrubb3@uw.edu>
2023-02-21 09:30:09 -0800

    chart3

--- Chart3.R
+++ Chart3.R
@@ -1,11 +1,10 @@
-setwd("/Users/kyle/INFO201")
-library("ggplot2")
-library("tidyverse")
-library("viridis")
-library("dplyr")
+install.packages("viridis")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("tidyverse")
 
 top_20_cities <- c("New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX", "Phillidelphia, PA", "Pheonix, AZ", "San Antonio, TX", "San Diego, CA", "Dallas, TX", "San Jose, CA", "Austin, TX", "Jacksonville, FL", "San Francisco, CA", "Indianapolis, IN", "Columbus, OH", "Fort Worth, TX", "Charlotte, NC", "Seattle, WA", "Denver, CO", "El Paso, TX")
-newdf <- read.csv("kyledf.csv", stringsAsFactors = F)
+  newdf <- read.csv("kyledf.csv", stringsAsFactors = F)
 dfone <- newdf %>% filter(str_detect(period_begin, "2013|2014|2015|2016|2017"))
 dftwo <- newdf %>% filter(str_detect(period_begin, "2018|2019|2020|2021|2022"))
 
@@ -18,5 +17,6 @@ maindf$year <- c('2013-2017', '2013-2017','2013-2017','2013-2017','2013-2017','2
 
 specie <- c(rep("NYC" , 2) , rep("LA" , 2) , rep("Chicago" , 2) , rep("Houston" , 2) , rep("Philly" , 2) , rep("Pheonix" , 2) , rep("San Antonio" , 2) , rep("San Diego" , 2),rep("Dallas" , 2) , rep("San Jose" , 2) , rep("Austin" , 2) , rep("Jacksonville" , 2), rep("San Francisco" , 2) , rep("Fort Worth" , 2) , rep("Charlotte" , 2) , rep("Seattle" , 2), rep("Denver" , 2) , rep("El Paso" , 2))
 ggplot(maindf, aes(x=region, y=days, fill=year)) +
-  geom_bar(stat="identity", position=position_dodge()) + coord_flip()
+  geom_bar(stat="identity", position=position_dodge()) + coord_flip() + ylab("20 Largest US Cities")
+
 
