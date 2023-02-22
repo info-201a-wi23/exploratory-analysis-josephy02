89a4879a81d8e3d2a6e67cd226d18b9d70f899c2
1lewis7 <121900269+1lewis7@users.noreply.github.com>
2023-02-21 18:46:57 -0800

    Grouped dataframes and created summary stats

--- tableSetup.R
+++ tableSetup.R
@@ -1,9 +1,3 @@
 kyle_summary <- read.csv("kyle_summary_df.csv", stringsAsFactors = FALSE)
-
 state_averages <- read.csv("state_averages.csv", stringsAsFactors = FALSE)
-
 joseph_summary <- read.csv("joseph_summary.csv", stringsAsFactors = FALSE)
-
-main_df <- left_join(kyle_summary, state_averages, by = "state")
-
-main_df <- left_join(main_df, joseph_summary, by = "state")
