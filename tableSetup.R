f9e83a988919a54bb8d960c3ad0484376fac34ed
1lewis7 <121900269+1lewis7@users.noreply.github.com>
2023-02-21 20:21:57 -0800

    Updated table.R

--- tableSetup.R
+++ tableSetup.R
@@ -1,3 +1,7 @@
 kyle_summary <- read.csv("kyle_summary_df.csv", stringsAsFactors = FALSE)
 state_averages <- read.csv("state_averages.csv", stringsAsFactors = FALSE)
 joseph_summary <- read.csv("joseph_summary.csv", stringsAsFactors = FALSE)
+
+head(kyle_summary)
+head(state_averages)
+head(joseph_summary)
