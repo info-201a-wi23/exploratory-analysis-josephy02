6e5169c178c0a06c52d9f8535052573cea9ba259
1lewis7 <121900269+1lewis7@users.noreply.github.com>
2023-02-21 17:26:50 -0800

    Fixed bugs in index.Rmd and Chart1.R

---
+++ Chart 1.R
@@ -0,0 +1,21 @@
+# Author: Lewis Going
+library("ggplot2")
+library("tidyverse")
+library("viridis")
+
+state_averages <- read.csv("state_averages.csv", stringsAsFactors = FALSE)
+
+# Scatter plot with regression line
+ggplot(state_averages, 
+       aes(x = avg_diff_ratio, 
+           y = avg_homes_sold)) +
+  geom_point(aes(colour = state),
+             size = 3) +
+  geom_smooth(method="lm") +
+  labs(x = "List vs sale ppsf ratio",
+       y = "Average homes sold",
+       title = "Correlation between homes sales and list vs sale price",
+       subtitle = "Average ratio between list and sale price-per-square-foot") +
+  scale_color_viridis(discrete = T) +
+  theme_minimal() +
+  theme(text = element_text(size=16))
